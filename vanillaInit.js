2e23743d69f64a9ce61117db749a5b3847f220f6
Lukáš Kovář <lkov@post.cz>
2022-01-14 21:18:16 +0100

    tangleMsgBox popup globally available

--- vanillaInit.js
+++ vanillaInit.js
@@ -1,17 +1,20 @@
 import { TangleDevice } from "./TangleDevice.js";
 import { TimeTrack } from "./TimeTrack.js";
 import { uint8ArrayToHexString } from "./functions.js";
+import TangleMsgBox from "../webcomponents/dialog-component.js";
 import "../../control.js";
 
 window.TangleDevice = TangleDevice;
 window.TimeTrack = TimeTrack;
 window.uint8ArrayToHexString = uint8ArrayToHexString;
+window.TangleMsgBox = TangleMsgBox;
 
 function injectScript(src) {
   const script = document.createElement("script");
   script.src = src;
   document.head.appendChild(script);
 }
+
 
 injectScript("code.js");
 // injectScript("control.js");
