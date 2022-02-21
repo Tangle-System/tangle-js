152ea3371a1bf207720a288461f08e9f207b9463
Matěj Suchánek <kkartk322@gmail.com>
2022-02-21 20:27:20 +0100

    fixing Nara Alpha legacy filter

--- TangleWebBluetoothConnector.js
+++ TangleWebBluetoothConnector.js
@@ -574,6 +574,7 @@ criteria example:
 
         // window.alert("add_legacy_uuids");
 
+        web_ble_options.filters.push({ name: "Nara Alpha" });
         web_ble_options.filters.push({ services: [this.FW_PRE_0_7_SERVICE_UUID] });
         web_ble_options.filters.push({ services: [this.FW_0_7_0_SERVICE_UUID] });
         web_ble_options.filters.push({ services: [this.FW_0_7_1_SERVICE_UUID] });
@@ -669,6 +670,7 @@ criteria example:
           if (!legacy_filters_applied) {
             legacy_filters_applied = true;
 
+            web_ble_options.filters.push({ name: "Nara Alpha" });
             web_ble_options.filters.push({ services: [this.FW_PRE_0_7_SERVICE_UUID] });
             web_ble_options.filters.push({ services: [this.FW_0_7_0_SERVICE_UUID] });
             web_ble_options.filters.push({ services: [this.FW_0_7_1_SERVICE_UUID] });
