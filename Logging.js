e84961571cf4821ddc9d8f34bf68c2ff03ea9cd6
Lukáš Kovář <lkov@post.cz>
2022-04-08 10:09:20 +0000

    make logging modifiers globally available

--- Logging.js
+++ Logging.js
@@ -7,16 +7,27 @@ export const DEBUG_LEVEL_VERBOSE = 5;
 
 export const logging = {
   error: console.error,
-  warn: function (...msg) { },
-  info: function (...msg) { },
-  debug: function (...msg) { },
-  verbose: function (...msg) { },
+  warn: function (...msg) {},
+  info: function (...msg) {},
+  debug: function (...msg) {},
+  verbose: function (...msg) {},
 };
 
 export function setLoggingLevel(level) {
-  logging.error = level >= 1 ? console.error : function (...msg) { };
-  logging.warn = level >= 2 ? console.warn : function (...msg) { };
-  logging.info = level >= 3 ? console.log : function (...msg) { };
-  logging.debug = level >= 4 ? console.log : function (...msg) { };
-  logging.verbose = level >= 5 ? console.log : function (...msg) { };
+  logging.error = level >= 1 ? console.error : function (...msg) {};
+  logging.warn = level >= 2 ? console.warn : function (...msg) {};
+  logging.info = level >= 3 ? console.log : function (...msg) {};
+  logging.debug = level >= 4 ? console.log : function (...msg) {};
+  logging.verbose = level >= 5 ? console.log : function (...msg) {};
+}
+
+if (window) {
+  window.DEBUG_LEVEL_NONE = DEBUG_LEVEL_NONE;
+  window.DEBUG_LEVEL_ERROR = DEBUG_LEVEL_ERROR;
+  window.DEBUG_LEVEL_WARN = DEBUG_LEVEL_WARN;
+  window.DEBUG_LEVEL_INFO = DEBUG_LEVEL_INFO;
+  window.DEBUG_LEVEL_DEBUG = DEBUG_LEVEL_DEBUG;
+  window.DEBUG_LEVEL_VERBOSE = DEBUG_LEVEL_VERBOSE;
+
+  window.setLoggingLevel = setLoggingLevel;
 }
