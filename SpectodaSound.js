8a4d2c3dd1d694434c48fa999a0d21d445175f86
Lukáš Kovář <lkov@post.cz>
2022-05-30 16:49:48 +0200

    spectodasound annotations

--- SpectodaSound.js
+++ SpectodaSound.js
@@ -7,6 +7,9 @@ export class SpectodaSound {
   #gain_node;
   #source;
   #audioContext;
+  /**
+   * @type {ScriptProcessorNode}
+   */
   #script_processor_get_audio_samples;
   #events;
   #fft;
@@ -46,7 +49,7 @@ export class SpectodaSound {
               this.#stream = stream;
               this.#source = this.#audioContext.createMediaStreamSource(this.#stream);
               resolve()
-              logging.debug('TangleSound.connect', 'Connected microphone');
+              logging.debug('SpectodaSound.connect', 'Connected microphone');
             },
             (e) => {
               alert('Error capturing audio.');
@@ -58,7 +61,7 @@ export class SpectodaSound {
     } else {
       this.#stream = mediaStream;
       this.#source = this.#audioContext.createMediaStreamSource(mediaStream);
-      logging.debug('TangleSound.connect', 'Connected mediaStream');
+      logging.debug('SpectodaSound.connect', 'Connected mediaStream');
     }
 
 
@@ -83,62 +86,12 @@ export class SpectodaSound {
     this.running = true;
     // var bufferCount = 0;
 
-    console.log("running samples")
+    console.log("running samples", this.BUFF_SIZE)
 
     // Tato funkce se provede pokaždé když dojde k naplnění bufferu o velikosti 2048 vzorků.
     // Při vzorkovacím kmitočku 48 kHz se tedy zavolá jednou za cca 42 ms.
-    this.#script_processor_get_audio_samples.onaudioprocess = (e) => {
-      // console.log("audio processing")
 
-      var samples = e.inputBuffer.getChannelData(0);
-      var rms_loudness_spectrum = 0;
-      this.#fft.forward(samples); //Vyypočtení fft ze vzorků.
-      var spectrum = this.#fft.spectrum; // Získání spektra o délce bufeer/2 v našem případě 1024 harmonických.
-
-      //--- Výpočet frekvence ---//
-      //
-      //    ((BufferSize/2)* Fvz)/BufferSize = Fmax
-      //    Fmax / (BufferSize/2) = Frekvence jednoho vzorku
-      // 
-      //------------------------//
-
-      // Zde se postupně sečte druhá mocnina všech 1024 vzorků.
-      spectrum.forEach(element => {
-        rms_loudness_spectrum += Math.pow(element, 2);
-      });
-
-      // for (let i = 30; i < 50; i++) {
-      //   rms_loudness_spectrum += Math.pow(spectrum[i],2);
-      // }
-
-      // Odmocnina součtu druhých mocnin nám dá efektivní hodnotu signálu "RMS"
-      rms_loudness_spectrum = Math.sqrt(rms_loudness_spectrum);
-
-      // Mapování efektivní hodnoty signálu na rozmezí 0-255 pro vhodný přenos dat.
-      // Zde je zejmána nutné dobře nastavit mapovací prahy. Spodní pro odstranění šumu okolí a horní nám udává výslednou dynamiku.
-      var out = mapValue(rms_loudness_spectrum, 0.00001, 0.9, 0, 255)
-
-      // console.log("spectrum avarge loudnes: "+ out);
-      // this.#handleControlSend(out);
-      this.#events.emit('loudness', out);
-      // logging.debug('loudness', out);
-
-      if (!this.running) {
-        this.#source.disconnect();
-        this.#gain_node.disconnect();
-      }
-
-      // if (bufferCount >= 5){
-      //     bufferCount = 0;
-      //     avarage_loudness = avarage_loudness/(BUFF_SIZE*6);
-      //     avarge_loudness_spectrum = avarge_loudness_spectrum/(BUFF_SIZE*3);
-
-      //     console.log("sample avarge loudnes: "+ mapValue(avarage_loudness,0.0005,0.05,0,255)); // This values set tresholds for noise and dynamics of signal.
-      //     console.log("spectrum avarge loudnes: "+ mapValue(avarge_loudness_spectrum, 0.00001, 0.0001, 0, 255));
-      //   } else {
-      //     bufferCount++;
-      // }
-    };
+    this.#script_processor_get_audio_samples.addEventListener('audioprocess', this.processHandler.bind(this));
   }
 
   stop() {
@@ -152,6 +105,61 @@ export class SpectodaSound {
   setBuffSize(size) {
     return this.BUFF_SIZE = size;
   }
+
+
+  processHandler(e) {
+    console.log("audio processing")
+
+    var samples = e.inputBuffer.getChannelData(0);
+    var rms_loudness_spectrum = 0;
+    this.#fft.forward(samples); //Vyypočtení fft ze vzorků.
+    var spectrum = this.#fft.spectrum; // Získání spektra o délce bufeer/2 v našem případě 1024 harmonických.
+
+    //--- Výpočet frekvence ---//
+    //
+    //    ((BufferSize/2)* Fvz)/BufferSize = Fmax
+    //    Fmax / (BufferSize/2) = Frekvence jednoho vzorku
+    // 
+    //------------------------//
+
+    // Zde se postupně sečte druhá mocnina všech 1024 vzorků.
+    spectrum.forEach(element => {
+      rms_loudness_spectrum += Math.pow(element, 2);
+    });
+
+    // for (let i = 30; i < 50; i++) {
+    //   rms_loudness_spectrum += Math.pow(spectrum[i],2);
+    // }
+
+    // Odmocnina součtu druhých mocnin nám dá efektivní hodnotu signálu "RMS"
+    rms_loudness_spectrum = Math.sqrt(rms_loudness_spectrum);
+
+    // Mapování efektivní hodnoty signálu na rozmezí 0-255 pro vhodný přenos dat.
+    // Zde je zejmána nutné dobře nastavit mapovací prahy. Spodní pro odstranění šumu okolí a horní nám udává výslednou dynamiku.
+    var out = mapValue(rms_loudness_spectrum, 0.00001, 0.9, 0, 255)
+
+    // console.log("spectrum avarge loudnes: "+ out);
+    // this.#handleControlSend(out);
+    this.#events.emit('loudness', out);
+    // logging.debug('loudness', out);
+
+    if (!this.running) {
+      this.#source.disconnect();
+      this.#gain_node.disconnect();
+    }
+
+    // if (bufferCount >= 5){
+    //     bufferCount = 0;
+    //     avarage_loudness = avarage_loudness/(BUFF_SIZE*6);
+    //     avarge_loudness_spectrum = avarge_loudness_spectrum/(BUFF_SIZE*3);
+
+    //     console.log("sample avarge loudnes: "+ mapValue(avarage_loudness,0.0005,0.05,0,255)); // This values set tresholds for noise and dynamics of signal.
+    //     console.log("spectrum avarge loudnes: "+ mapValue(avarge_loudness_spectrum, 0.00001, 0.0001, 0, 255));
+    //   } else {
+    //     bufferCount++;
+    // }
+  }
+
   // this.#events.emit('control', {
   //   type: 'loudness',
   //   value: value
