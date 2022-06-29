909436b6df7c65c89745ebe4779e65b5626ae2d3
Lukáš Kovář <lkov@post.cz>
2022-06-29 12:31:30 +0000

    sk

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -44,7 +44,7 @@ export default {
   lineEnding: "auto",
   // Control the line ending. See options at https://github.com/ryanve/eol
 
-  locales: ["en", "cs"],
+  locales: ["en", "cs", "sk"],
   // An array of the locales in your applications
 
   namespaceSeparator: "___",
